1968a3e45ec2e7b73d45045c1a5cb249a89ffbdc
Dimitar <bulgaria_mitko@yahoo.com>
2022-07-13 22:26:05 +0300

    added custom webview - custom function for FlutterFlow

---
+++ custom-webview-ff.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:webviewx/webviewx.dart';
+
+class CustomWebView extends StatefulWidget {
+  const CustomWebView({
+    Key key,
+    @required this.url,
+    this.width,
+    this.height,
+    this.bypass = false,
+    this.horizontalScroll = false,
+    this.verticalScroll = false,
+  }) : super(key: key);
+
+  final bool bypass;
+  final bool horizontalScroll;
+  final bool verticalScroll;
+  final double height;
+  final double width;
+  final String url;
+
+  @override
+  _CustomWebViewState createState() => _CustomWebViewState();
+}
+
+class _CustomWebViewState extends State<CustomWebView> {
+  @override
+  Widget build(BuildContext context) => WebViewX(
+        key: webviewKey,
+        width: widget.width ?? MediaQuery.of(context).size.width,
+        height: widget.height ?? MediaQuery.of(context).size.height,
+        ignoreAllGestures: false,
+        initialContent: widget.url,
+        initialMediaPlaybackPolicy:
+            AutoMediaPlaybackPolicy.requireUserActionForAllMediaTypes,
+        initialSourceType:
+            widget.bypass ? SourceType.urlBypass : SourceType.url,
+        javascriptMode: JavascriptMode.unrestricted,
+        webSpecificParams: const WebSpecificParams(
+          webAllowFullscreenContent: true,
+        ),
+        mobileSpecificParams: MobileSpecificParams(
+          debuggingEnabled: false,
+          gestureNavigationEnabled: true,
+          mobileGestureRecognizers: {
+            if (widget.verticalScroll)
+              Factory<VerticalDragGestureRecognizer>(
+                () => VerticalDragGestureRecognizer(),
+              ),
+            if (widget.horizontalScroll)
+              Factory<HorizontalDragGestureRecognizer>(
+                () => HorizontalDragGestureRecognizer(),
+              ),
+          },
+          androidEnableHybridComposition: true,
+        ),
+      );
+
+  Key get webviewKey => Key(
+        [
+          widget.url,
+          widget.width,
+          widget.height,
+          widget.bypass,
+          widget.horizontalScroll,
+          widget.verticalScroll,
+        ].map((s) => s?.toString() ?? '').join(),
+      );
+}

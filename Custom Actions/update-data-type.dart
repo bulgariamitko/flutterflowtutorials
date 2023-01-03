938ff077a84d31a7d4017d456d75d859539d6a7b
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2023-01-03 09:15:40 +0100

    change data type and upload any file type

---
+++ Custom Actions/update-data-type.dart
@@ -0,0 +1,32 @@
+// code created by https://www.youtube.com/@flutterflowexpert
+
+Future changeDataType(
+  List<CarStruct>? car,
+  DocumentReference? carRef,
+) async {
+  // Add your function code here!
+  carRef = carRef ?? FirebaseFirestore.instance.doc('/cars/4343t34t3434r');
+
+  CarStruct myCar = createCarStruct(
+      onSale: true,
+      brand: "Brand",
+      color: Color.fromARGB(74, 15, 209, 129),
+      doors: 4);
+
+  final removeCar = {
+    FieldValue.arrayRemove([myCar]),
+  };
+
+  CarStruct myNewCar = createCarStruct(
+      onSale: true,
+      brand: "New Car",
+      color: Color.fromRGBO(45, 45, 45, 0.3),
+      doors: 4);
+
+  final addCar = {
+    FieldValue.arrayUnion([myNewCar]),
+  };
+
+  await carRef.update({'name': "newName", 'car': removeCar});
+  await carRef.update({'name': "newName", 'car': addCar});
+}

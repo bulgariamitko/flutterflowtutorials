7e16868c94c62947e7510aff0ea16f03ebe9fc5c
Dimitar <bulgaria_mitko@yahoo.com>
2023-01-04 23:34:52 +0100

    added code for uploading multy file types

---
+++ Custom Actions/upload-multi-any-file-type.dart
@@ -0,0 +1,38 @@
+// code created by https://www.youtube.com/@flutterflowexpert
+
+import '../../backend/firebase_storage/storage.dart';
+import '../../flutter_flow/flutter_flow_widgets.dart';
+import '../../flutter_flow/upload_media.dart';
+
+Future uploadAnyFileType(BuildContext context, List<String>? fileType) async {
+  fileType = fileType ?? ['pdf'];
+
+  final selectedFile = await selectFile(allowedExtensions: fileType);
+  if (selectedFile != null) {
+    showUploadMessage(
+      context,
+      'Uploading file...',
+      showLoading: true,
+    );
+    final downloadUrl =
+        await uploadData(selectedFile.storagePath, selectedFile.bytes);
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    if (downloadUrl != null) {
+      // save url to local state
+      FFAppState().update(() {
+        FFAppState().filePath = downloadUrl;
+      });
+
+      showUploadMessage(
+        context,
+        'Success!',
+      );
+    } else {
+      showUploadMessage(
+        context,
+        'Failed to upload file',
+      );
+      return;
+    }
+  }
+}

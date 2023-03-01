f40b68c7eb514d90c7dd4654ee32463d617fecc0
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2023-03-01 17:52:40 +0100

    added code for my live session q&a

--- Videos/230301-live.dart
+++ Videos/230301-live.dart
@@ -1,3 +1,7 @@
+// code created by https://www.youtube.com/@flutterflowexpert
+// video - https://www.youtube.com/watch?v=_TCVr5R_i78
+// if you have problem implementing this code you can hire me as a mentor - https://calendly.com/bulgaria_mitko
+
 // how to compare two images using bytes, method 1
 import 'dart:ui' as ui;
 import 'dart:io';

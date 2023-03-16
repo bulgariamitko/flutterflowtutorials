58c45add4fb80c854cf77e8efa198f672b6ac50a
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2023-03-16 16:56:10 +0100

    speech to text custom action

---
+++ Custom Actions/speak-to-me.dart
@@ -0,0 +1,59 @@
+// code created by https://www.youtube.com/@flutterflowexpert
+// video - https://youtu.be/6BCOl4w0mcI
+// if you have problem implementing this code you can hire me as a mentor - https://calendly.com/bulgaria_mitko
+
+import 'dart:async';
+import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+
+Future speechToText() async {
+  String output = '';
+  bool _onDevice = false;
+  final TextEditingController _pauseForController =
+      TextEditingController(text: '3');
+  final TextEditingController _listenForController =
+      TextEditingController(text: '30');
+  double minSoundLevel = 50000;
+  double maxSoundLevel = -50000;
+  String _currentLocaleId = '';
+  final SpeechToText speech = SpeechToText();
+
+  bool isInitialized = await speech.initialize();
+
+  if (isInitialized) {
+    var systemLocale = await speech.systemLocale();
+    _currentLocaleId = systemLocale?.localeId ?? '';
+    final pauseFor = int.tryParse(_pauseForController.text);
+    final listenFor = int.tryParse(_listenForController.text);
+    speech.listen(
+      onResult: (result) {
+        if (!result.finalResult) {
+          FFAppState().update(() {
+            FFAppState().btnTalk = 'listening...';
+            FFAppState().stt = '${result.recognizedWords}';
+          });
+        } else {
+          FFAppState().update(() {
+            output = '${result.recognizedWords}';
+            FFAppState().sstSendText = '${result.recognizedWords}';
+            FFAppState().btnTalk = 'Talk';
+          });
+        }
+      },
+      listenFor: Duration(seconds: listenFor ?? 30),
+      pauseFor: Duration(seconds: pauseFor ?? 3),
+      partialResults: true,
+      localeId: _currentLocaleId,
+      onSoundLevelChange: (level) {
+        minSoundLevel = min(minSoundLevel, level);
+        maxSoundLevel = max(maxSoundLevel, level);
+        print('sound level $level: $minSoundLevel - $maxSoundLevel ');
+        level = level;
+      },
+      cancelOnError: true,
+      listenMode: ListenMode.confirmation,
+      onDevice: _onDevice,
+    );
+  }
+}

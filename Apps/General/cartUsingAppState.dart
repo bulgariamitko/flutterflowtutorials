43cbf5ee7229ee9a36e4fddf156e3cbcfd1fde20
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2023-03-25 10:51:12 +0100

    upload files to firebase by raw bytes and fixed names of some files

---
+++ Apps/General/cartUsingAppState.dart
@@ -0,0 +1,94 @@
+// code created by https://www.youtube.com/@flutterflowexpert
+// video - no
+// if you have problem implementing this code you can hire me as a mentor - https://calendly.com/bulgaria_mitko
+
+Future calTotal(
+  String? productid,
+  bool? plus,
+  String? name,
+  String? price,
+) async {
+  // null safety
+  productid ??= '0';
+  plus ??= true;
+  name ??= 'name';
+  price ??= '0';
+
+  bool addItemInList = true;
+  FFAppState().Total = 0;
+  for (int i = 0; i < FFAppState().prices.length; i++) {
+    // if the product is already in the list
+    if (FFAppState().productsids[i] == productid) {
+      print(['exist', productid, FFAppState().productsids[i], name, price]);
+      if (plus) {
+        print('+');
+        FFAppState().qttys[i] =
+            (int.parse(FFAppState().qttys[i]) + 1).toString();
+
+        addItemInList = false;
+        break;
+      } else {
+        int newQuantity = int.parse(FFAppState().qttys[i]) - 1;
+
+        FFAppState().qttys[i] =
+            (int.parse(FFAppState().qttys[i]) - 1).toString();
+        // remove the item if qtty is 0
+        if (newQuantity <= 0) {
+          print('remove');
+          FFAppState().productName.removeAt(i);
+          FFAppState().prices.removeAt(i);
+          FFAppState().qttys.removeAt(i);
+          FFAppState().productsids.removeAt(i);
+
+          addItemInList = false;
+          break;
+        } else {
+          print('-');
+          FFAppState().qttys[i] = newQuantity.toString();
+
+          addItemInList = false;
+          break;
+        }
+      }
+    }
+  }
+
+  // add a new product in cart
+  if ((FFAppState().prices.length == 0 || addItemInList) && plus != false) {
+    print('add a new item');
+    FFAppState().prices.add(price);
+    FFAppState().qttys.add('1');
+    FFAppState().productName.add(name);
+    FFAppState().procudtTotal.add(price);
+    FFAppState().productsids.add(productid);
+
+    // cal product total
+    FFAppState().procudtTotal[0] = (double.parse(FFAppState().prices[0]) *
+            int.parse(FFAppState().qttys[0]))
+        .toString();
+
+    // cal order total
+    FFAppState().Total +=
+        int.parse(FFAppState().qttys[0]) * double.parse(FFAppState().prices[0]);
+  }
+
+  if (FFAppState().prices.length == 1 &&
+      plus == false &&
+      FFAppState().qttys[0] == 1) {
+    print('remove when only 1 item');
+    FFAppState().productName.removeAt(0);
+    FFAppState().prices.removeAt(0);
+    FFAppState().qttys.removeAt(0);
+    FFAppState().productsids.removeAt(0);
+  }
+
+  for (int i = 0; i < FFAppState().prices.length; i++) {
+    // cal product total
+    FFAppState().procudtTotal[i] = (double.parse(FFAppState().prices[i]) *
+            int.parse(FFAppState().qttys[i]))
+        .toString();
+    // cal order total
+    FFAppState().Total +=
+        int.parse(FFAppState().qttys[i]) * double.parse(FFAppState().prices[i]);
+  }
+}

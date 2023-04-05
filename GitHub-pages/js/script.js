7a3a2c3d77dfdb2f6da6fb66c786ffadf208c69c
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2023-04-06 00:19:37 +0200

    git pages, first btn

--- GitHub-pages/js/script.js
+++ GitHub-pages/js/script.js
@@ -1,1 +1,7 @@
-console.log(3232);
+$("#copy-text-btn").click(function() {
+  var textToCopy = $("#text-to-copy");
+  textToCopy.focus();
+  textToCopy.select();
+  document.execCommand("copy");
+  alert("Text copied: " + textToCopy.val());
+});

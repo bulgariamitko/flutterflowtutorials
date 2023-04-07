7f6c7fb72a97edc4a07ce4a13e9e5740288392f2
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2023-04-08 00:42:48 +0200

    gh-pages, added navbar and search

--- gh-pages/js/script.js
+++ gh-pages/js/script.js
@@ -1,4 +1,55 @@
+function setDarkMode(enabled) {
+    if (enabled) {
+      $('body').addClass('bg-dark text-white');
+      $('.navbar').addClass('navbar-dark bg-dark');
+      $('#modeToggle .fa-sun').addClass('d-none');
+      $('#modeToggle .fa-moon').removeClass('d-none');
+    } else {
+      $('body').removeClass('bg-dark text-white');
+      $('.navbar').removeClass('navbar-dark bg-dark');
+      $('#modeToggle .fa-sun').removeClass('d-none');
+      $('#modeToggle .fa-moon').addClass('d-none');
+    }
+  }
+
+function displayResults(data) {
+  searchResults.innerHTML = '';
+  data.forEach((item) => {
+    const video = document.createElement('div');
+    video.innerHTML = `
+      <h3>${item.title}</h3>
+      <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" src="${item.url}" title="${item.title}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+    `;
+    searchResults.appendChild(video);
+  });
+}
+
 $(document).ready(function() {
+  let darkModeEnabled = localStorage.getItem('darkMode') === 'true' ? true : false;
+
+  setDarkMode(darkModeEnabled);
+
+  $('#modeToggle').on('click', function() {
+    darkModeEnabled = !darkModeEnabled;
+    localStorage.setItem('darkMode', darkModeEnabled);
+    setDarkMode(darkModeEnabled);
+  });
+
+  const searchInput = document.querySelector('.fake-input');
+	const searchResults = document.getElementById('searchResults');
+
+	searchInput.addEventListener('input', (e) => {
+	  const query = e.target.value.toLowerCase();
+	  fetch('gh-pages/data/data.json')
+	    .then((response) => response.json())
+	    .then((data) => {
+	      const filteredData = data.filter((item) => item.title.toLowerCase().includes(query));
+	      displayResults(filteredData);
+	    });
+	});
+
 	$.getJSON('gh-pages/data/data.json', function(data) {
         $.each(data, function(index, video) {
           var videoElement = `

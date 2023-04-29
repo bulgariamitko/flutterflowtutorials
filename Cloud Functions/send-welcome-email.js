86cd39e967a9f123cbe1ddf603a686430a0bbd06
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2023-04-29 10:47:56 +0200

    fixed email video link

--- Cloud Functions/send-welcome-email.js
+++ Cloud Functions/send-welcome-email.js
@@ -1,5 +1,5 @@
 // code created by https://www.youtube.com/@flutterflowexpert
-// video - https://www.youtube.com/watch?v=zVc4UuIT6Gk
+// video - https://www.youtube.com/watch?v=ZVCAw2JHZ_Y
 // replace - [{"Main domain": "mail.example.com"}, {"Sender email": "demo@example.com"}, {"Email password": "myPassword"}, {"Field name to display name": "displayName"}]
 // support my work - https://github.com/sponsors/bulgariamitko
 

435a81da2e03fc921bd68cf533a22ee1cbe0749a
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2023-05-07 14:05:06 +0200

    combine two list of strings

---
+++ Custom Functions/combine-two-lists.dart
@@ -0,0 +1,15 @@
+// code created by https://www.youtube.com/@flutterflowexpert
+// video - no
+// support my work - https://github.com/sponsors/bulgariamitko
+
+List<String> combineLists(List<String> list1, List<String> list2) {
+  List<String> combinedList = [];
+
+  // Add all elements from the first list
+  combinedList.addAll(list1);
+
+  // Add all elements from the second list
+  combinedList.addAll(list2);
+
+  return combinedList;
+}

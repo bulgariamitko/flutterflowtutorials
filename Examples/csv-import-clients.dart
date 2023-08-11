fa8791f6dcf5f9c3a53bd4ad3e4e8a2375d19483
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2023-08-11 09:57:54 +0200

    added some more example code

---
+++ Examples/csv-import-clients.dart
@@ -0,0 +1,100 @@
+// code created by https://www.youtube.com/@flutterflowexpert
+// video - no
+// support my work - https://github.com/sponsors/bulgariamitko
+
+import 'dart:io';
+
+// import 'dart:convert' show utf8;
+import 'dart:convert';
+import 'package:csv/csv.dart';
+// import '../../backend/firebase_storage/storage.dart';
+// import '../../flutter_flow/flutter_flow_widgets.dart';
+import '../../flutter_flow/upload_data.dart';
+
+Future getTitlesFromCSVFile(
+  BuildContext context,
+  String? divider,
+  int? titleRow,
+) async {
+  // null safety check
+  divider = divider ?? ',';
+  // collectionName ??= 'users';
+  // fieldName1 ??= 'error';
+  // fieldName2 ??= 'error';
+  // fieldName3 ??= 'error';
+  // fieldName4 ??= 'error';
+  // fieldName5 ??= 'error';
+  titleRow ??= 0;
+
+  // // Get a reference to the Firestore database
+  // final firestore = FirebaseFirestore.instance;
+
+  // // TODO: Change collection name
+  // final collectionRef = firestore.collection(collectionName);
+
+  final selectedFile = await selectFile(allowedExtensions: ['csv']);
+
+  if (selectedFile != null) {
+    showUploadMessage(
+      context,
+      'Uploading file...',
+      showLoading: true,
+    );
+
+    final fileString = utf8.decode(selectedFile.bytes);
+
+    List<String> rows = [];
+    // String fileType = 'csv';
+    // Map<String, dynamic> doc = {};
+    // if file is json
+    rows = fileString.split('\n');
+
+    FFAppState().update(() {
+      FFAppState().csvTitles = rows[(titleRow ?? 0) - 1].split(divider ?? ',');
+      print([FFAppState().csvTitles]);
+    });
+
+    int i = 0;
+    for (var row in rows) {
+      i++;
+      // skip the head fields
+      if (i <= titleRow || i == rows.length - 2) {
+        continue;
+      }
+
+      // print(['v4', row]);
+
+      List<String> fields = row.split(divider);
+
+      print(['Fields: ', fields.length, fields]); // Debugging line
+
+      if (fields.length >= 12) {
+        FFAppState().update(() {
+          FFAppState().dumpFieldName0.add(fields[0]);
+          FFAppState().dumpFieldName1.add(fields[1]);
+          FFAppState().dumpFieldName2.add(fields[2]);
+          FFAppState().dumpFieldName3.add(fields[3]);
+          FFAppState().dumpFieldName4.add(fields[4]);
+          FFAppState().dumpFieldName5.add(fields[5]);
+          FFAppState().dumpFieldName6.add(fields[6]);
+          FFAppState().dumpFieldName7.add(fields[7]);
+          FFAppState().dumpFieldName8.add(fields[8]);
+          FFAppState().dumpFieldName9.add(fields[9]);
+          FFAppState().dumpFieldName10.add(fields[10]);
+          FFAppState().dumpFieldName11.add(fields[11]);
+        });
+      }
+    }
+
+    showUploadMessage(
+      context,
+      'Success!',
+    );
+  }
+}
+
+// Set your action name, define your arguments and return parameter,
+// and then add the boilerplate code using the button on the right!
+
+// Set your action name, define your arguments and return parameter,
+// and then add the boilerplate code using the button on the right!
---
+++ Examples/update-or-insert-doc-using-filter.dart
@@ -0,0 +1,54 @@
+// code created by https://www.youtube.com/@flutterflowexpert
+// video - no
+// support my work - https://github.com/sponsors/bulgariamitko
+
+Future updateOrInsertDocUsingFilter(
+  String? collectionName,
+  DocumentReference userRef,
+) async {
+  // null check
+  collectionName = collectionName ?? '';
+
+  // Get a reference to the Firestore database
+  final firestore = FirebaseFirestore.instance;
+
+  // Get a reference to the collection
+  final collectionRef = firestore.collection(collectionName);
+
+  // Insert the new documents in the collection
+  for (int i = 0; i < FFAppState().fieldName0.length; i++) {
+    final doc = {
+      'product_foto1': FFAppState().fieldName0[i] ?? '',
+      'product_foto2': FFAppState().fieldName1[i] ?? '',
+      'product_foto3': FFAppState().fieldName2[i] ?? '',
+      'name': FFAppState().fieldName3[i] ?? '',
+      'nroPieza': FFAppState().fieldName4[i] ?? '',
+      'SKU': FFAppState().fieldName5[i] ?? '',
+      'barcode': FFAppState().fieldName6[i] ?? '',
+      'quantity': FFAppState().fieldName7[i] ?? '',
+      'price': FFAppState().fieldName8[i] ?? '',
+      'equivalencias': FFAppState().fieldName9[i] ?? '',
+      'marcaProducto': FFAppState().fieldName10[i] ?? '',
+      'modeloProducto': FFAppState().fieldName11[i] ?? '',
+      'uid': userRef,
+      'activa': true,
+      'activa_byuser': false,
+      'created_at': DateTime.now(),
+    };
+
+
+    // TODO: change fields you want to search for
+    final docFilter = await collectionRef
+        .where('SKU', isEqualTo: FFAppState().fieldName5[i])
+        .where('uid', isEqualTo: userRef)
+        .get();
+
+    if (docFilter.docs.isNotEmpty) {
+      // Update the existing document with the new data
+      await docFilter.docs.first.reference.update(doc);
+    } else {
+      // Add a new document to the collection
+      await collectionRef.add(doc);
+    }
+  }
+}

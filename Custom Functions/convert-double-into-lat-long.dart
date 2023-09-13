cb6ea808726b1c921fc0d81b69a17c635c394f78
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2023-09-13 09:41:45 +0200

    added code to convert longlng to array of doubles and also fixed some of the comments

---
+++ Custom Functions/convert-double-into-lat-long.dart
@@ -0,0 +1,23 @@
+// YouTube channel - https://www.youtube.com/@flutterflowexpert
+// video - no
+// Join the Klaturov army - https://www.youtube.com/@flutterflowexpert/join
+// Support my work - https://github.com/sponsors/bulgariamitko
+// Website - https://bulgariamitko.github.io/flutterflowtutorials/
+// You can book me as FF mentor - https://calendly.com/bulgaria_mitko
+// GitHub repo - https://github.com/bulgariamitko/flutterflowtutorials
+// Discord channel - https://discord.gg/ERDVFBkJmY
+
+LatLng converDoubleToLatLong(
+  double? lat,
+  double? long,
+) {
+  /// MODIFY CODE ONLY BELOW THIS LINE
+
+  // null safety
+  lat ??= 0.0;
+  long ??= 0.0;
+
+  return LatLng(lat, long);
+
+  /// MODIFY CODE ONLY ABOVE THIS LINE
+}
---
+++ Custom Functions/convert-lat-long-into-doubles.dart
@@ -0,0 +1,19 @@
+// YouTube channel - https://www.youtube.com/@flutterflowexpert
+// video - no
+// Join the Klaturov army - https://www.youtube.com/@flutterflowexpert/join
+// Support my work - https://github.com/sponsors/bulgariamitko
+// Website - https://bulgariamitko.github.io/flutterflowtutorials/
+// You can book me as FF mentor - https://calendly.com/bulgaria_mitko
+// GitHub repo - https://github.com/bulgariamitko/flutterflowtutorials
+// Discord channel - https://discord.gg/ERDVFBkJmY
+
+List<double> converLatLongToDouble(LatLng? location) {
+  /// MODIFY CODE ONLY BELOW THIS LINE
+
+// null safety
+  location ??= LatLng(0.0, 0.0);
+
+  return [location.latitude, location.longitude];
+
+  /// MODIFY CODE ONLY ABOVE THIS LINE
+}

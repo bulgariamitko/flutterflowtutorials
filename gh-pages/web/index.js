1e6ec9ad6054f28dbfe07b2bd97cfd2281abd2c2
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2023-12-15 00:07:51 +0100

    web deployant loading in headers

--- gh-pages/web/index.js
+++ gh-pages/web/index.js
@@ -1,1 +1,50 @@
-console.log('this is a test!!!');
+// Create the overlay and image elements
+const overlay = document.createElement('div');
+overlay.id = 'loader';
+overlay.style.cssText = `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.7);
+    z-index: 999;
+`;
+
+const image = document.createElement('img');
+image.id = 'image';
+image.src = 'https://bulgariamitko.github.io/flutterflowtutorials/gh-pages/web/loading.png';
+image.style.cssText = `
+    max-width: 100%;
+    max-height: 100%;
+    display: none;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+image.style.display = 'none';
+
+// Append the elements to the body
+document.body.appendChild(overlay);
+document.body.appendChild(image);
+
+// Function to hide the overlay and display the GIF
+function hideOverlay() {
+    overlay.style.display = 'none';
+    image.style.display = 'block';
+}
+
+// Add an event listener to hide the overlay when all external JS files are loaded
+document.addEventListener('DOMContentLoaded', () => {
+    // Replace the following lines with the actual code that loads your external JS files
+    // For demonstration purposes, we'll use a setTimeout to simulate loading external JS files.
+    setTimeout(hideOverlay, 100); // Replace with your actual loading code.
+});
+
+// Fallback: If all external resources are loaded and the DOMContentLoaded event doesn't fire,
+// we'll still hide the overlay when the window's load event is triggered.
+window.addEventListener('load', hideOverlay);

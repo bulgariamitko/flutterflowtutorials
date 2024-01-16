afc6f10e06cf6ed70956e1d0c78c082a4e979396
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2024-01-16 10:39:48 +0100

    Add YouTube channel and support links

--- Cloud Functions/cascade-delete/index.js
+++ Cloud Functions/cascade-delete/index.js
@@ -1,3 +1,12 @@
+// YouTube channel - https://www.youtube.com/@flutterflowexpert
+// video - no
+// Join the Klaturov army - https://www.youtube.com/@flutterflowexpert/join
+// Support my work - https://github.com/sponsors/bulgariamitko
+// Website - https://bulgariamitko.github.io/flutterflowtutorials/
+// You can book me as FF mentor - https://calendly.com/bulgaria_mitko
+// GitHub repo - https://github.com/bulgariamitko/flutterflowtutorials
+// Discord channel - https://discord.gg/ERDVFBkJmY
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 // To avoid deployment errors, do not call admin.initializeApp() in your code
@@ -16,6 +25,13 @@ exports.cleanupUserData = functions.firestore
             'chat_messages': 'user',
             'notifications': 'notification_for'
         };
+
+        try {
+            await admin.auth().deleteUser(userId);
+            console.log('Successfully deleted user from Firebase Authentication');
+        } catch (error) {
+            console.error('Error deleting user from Firebase Authentication:', error);
+        }
 
         // Handling deletion of list of documents
         const usersSnapshot = await db.collection('orders').where('users', 'array-contains', userRef).get();
@@ -40,6 +56,13 @@ exports.cleanupUserData = functions.firestore
                 });
             }
         };
+
+        // Delete the 'fcm_tokens' sub-collection and its documents
+        const fcmTokensQuerySnapshot = await userRef.collection('fcm_tokens').get();
+        fcmTokensQuerySnapshot.forEach(tokenDoc => {
+            console.log('Deleting document in fcm_tokens with ID:', tokenDoc.id);
+            tokenDoc.ref.delete();
+        });
 
         // Iterate over the collections and fields
         for (const [collection, field] of Object.entries(references)) {

7d53b3f221e3125d4aa2ee34f35e5932459b3a2f
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2024-01-16 19:46:33 +0100

    Add contact information and links to the code file

--- Cloud Functions/del-user-as-admin/index.js
+++ Cloud Functions/del-user-as-admin/index.js
@@ -1,3 +1,12 @@
+// YouTube channel - https://www.youtube.com/@flutterflowexpert
+// video - no
+// Join the Klaturov army - https://www.youtube.com/@flutterflowexpert/join
+// Support my work - https://github.com/sponsors/bulgariamitko
+// Website - https://bulgariamitko.github.io/flutterflowtutorials/
+// You can book me as FF mentor - https://calendly.com/bulgaria_mitko
+// GitHub repo - https://github.com/bulgariamitko/flutterflowtutorials
+// Discord channel - https://discord.gg/ERDVFBkJmY
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 // To avoid deployment errors, do not call admin.initializeApp() in your code

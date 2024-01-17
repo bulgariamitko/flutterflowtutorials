ff3ad99920c206245532e62e44aa28b8ae75cb44
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2024-01-17 22:44:34 +0100

    Add custom app state and related actions

---
+++ Custom Actions/custom app state/custom-app-state.dart
@@ -0,0 +1,48 @@
+// YouTube channel - https://www.youtube.com/@flutterflowexpert
+// video - no
+// Join the Klaturov army - https://www.youtube.com/@flutterflowexpert/join
+// Support my work - https://github.com/sponsors/bulgariamitko
+// Website - https://bulgariamitko.github.io/flutterflowtutorials/
+// You can book me as FF mentor - https://calendly.com/bulgaria_mitko
+// GitHub repo - https://github.com/bulgariamitko/flutterflowtutorials
+// Discord channel - https://discord.gg/ERDVFBkJmY
+
+CustomAppState appState = CustomAppState();
+
+Future customAppState() async {
+  // CustomAppState appState = CustomAppState();
+}
+
+class CustomAppState extends ChangeNotifier {
+  List<UsersRecord> _usersList = [];
+
+  List<UsersRecord> get usersList => _usersList;
+
+  set usersList(List<UsersRecord> value) {
+    if (_usersList != value) {
+      _usersList = value;
+      notifyListeners(); // Notify listeners when the value changes
+    }
+  }
+
+  // Method to add a UsersRecord to the list
+  void addUser(UsersRecord user) {
+    _usersList.add(user);
+    notifyListeners();
+  }
+
+  // Method to remove a UsersRecord from the list
+  void removeUser(UsersRecord user) {
+    _usersList.remove(user);
+    notifyListeners();
+  }
+
+  // Method to add a list of UsersRecord to the list
+  void addUsers(List<UsersRecord> users) {
+    _usersList.addAll(users);
+    notifyListeners();
+  }
+}
+
+// Set your action name, define your arguments and return parameter,
+// and then add the boilerplate code using the green button on the right!
---
+++ Custom Actions/custom app state/set-custom-app-state.dart
@@ -0,0 +1,15 @@
+// YouTube channel - https://www.youtube.com/@flutterflowexpert
+// video - no
+// Join the Klaturov army - https://www.youtube.com/@flutterflowexpert/join
+// Support my work - https://github.com/sponsors/bulgariamitko
+// Website - https://bulgariamitko.github.io/flutterflowtutorials/
+// You can book me as FF mentor - https://calendly.com/bulgaria_mitko
+// GitHub repo - https://github.com/bulgariamitko/flutterflowtutorials
+// Discord channel - https://discord.gg/ERDVFBkJmY
+
+import 'package:whats_new2/custom_code/actions/custom_app_state.dart';
+
+Future setCustomAppState(List<UsersRecord> data) async {
+  // Use the global instance to set the users list
+  appState.usersList = data;
+}

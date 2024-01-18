01fa0c1acec5d54339480fc412aa63a5295a1c04
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2024-01-18 17:34:25 +0100

    Add links and contact information to SQL files

--- SQLite/update.sql
+++ SQLite/update.sql
@@ -1,3 +1,12 @@
+-- // YouTube channel - https://www.youtube.com/@flutterflowexpert
+-- // paid video - https://youtu.be/yl1rn79KpZo
+-- // Join the Klaturov army - https://www.youtube.com/@flutterflowexpert/join
+-- // Support my work - https://github.com/sponsors/bulgariamitko
+-- // Website - https://bulgariamitko.github.io/flutterflowtutorials/
+-- // You can book me as FF mentor - https://calendly.com/bulgaria_mitko
+-- // GitHub repo - https://github.com/bulgariamitko/flutterflowtutorials
+-- // Discord channel - https://discord.gg/ERDVFBkJmY
+
 INSERT INTO Players (name, position, team_id)
 VALUES ('${name}', '${position}', ${teamid})
 

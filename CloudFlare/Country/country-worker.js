dad33a36e74a71222904f0935e581a0c8e8df8b8
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2024-03-29 23:54:27 +0200

    updated cf code

--- CloudFlare/Country/country-worker.js
+++ CloudFlare/Country/country-worker.js
@@ -1,12 +1,11 @@
 // YouTube channel - https://www.youtube.com/@flutterflowexpert
-// video - no
+// paid video - https://youtu.be/Y82DsIHiPNI
 // Join the Klaturov army - https://www.youtube.com/@flutterflowexpert/join
 // Support my work - https://github.com/sponsors/bulgariamitko
 // Website - https://bulgariamitko.github.io/flutterflowtutorials/
 // You can book me as FF mentor - https://calendly.com/bulgaria_mitko
 // GitHub repo - https://github.com/bulgariamitko/flutterflowtutorials
 // Discord channel - https://discord.gg/G69hSUqEeU
-
 
 async function handleRequest(request) {
     const country = request.headers.get('CF-IPCountry');
---
+++ CloudFlare/Country/fetch-country.dart
@@ -0,0 +1,24 @@
+// YouTube channel - https://www.youtube.com/@flutterflowexpert
+// paid video - https://youtu.be/Y82DsIHiPNI
+// Join the Klaturov army - https://www.youtube.com/@flutterflowexpert/join
+// Support my work - https://github.com/sponsors/bulgariamitko
+// Website - https://bulgariamitko.github.io/flutterflowtutorials/
+// You can book me as FF mentor - https://calendly.com/bulgaria_mitko
+// GitHub repo - https://github.com/bulgariamitko/flutterflowtutorials
+// Discord channel - https://discord.gg/G69hSUqEeU
+
+import 'package:http/http.dart' as http;
+
+Future<dynamic> fetchCountry() async {
+  final response = await http
+      .get(Uri.parse('https://location-country.bulgaria-mitko.workers.dev/'));
+
+  dynamic data = {'country': 'no location'};
+
+  if (response.statusCode == 200) {
+    // If server returns an OK response, parse the JSON
+    data = jsonDecode(response.body);
+  }
+
+  return data;
+}

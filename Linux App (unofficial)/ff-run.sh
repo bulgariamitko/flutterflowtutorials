a1d381a86cfe65616e8643b28235347911ddafc5
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2024-06-11 23:04:06 +0300

    updated linux app, now it is downloading the assets and the folder name will always work, no more error not finding the folder. use the new version 0.5

--- Linux App (unofficial)/ff-run.sh
+++ Linux App (unofficial)/ff-run.sh
@@ -15,9 +15,14 @@ if [ -z "$project_id" ]; then
     exit 1
 fi
 
+# Get the first part of the project ID (prefix before any dash)
+project_prefix=$(echo "$project_id" | awk -F'-' '{print $1}')
+project_dir_name=$(find . -maxdepth 1 -type d -name "${project_prefix}*" | head -n 1)
+project_dir_name=${project_dir_name#./}
+
 if [ "$is_flutter_running" = "true" ]; then
     echo "Flutter is reported to be running. Only executing FlutterFlow export-code..."
-    flutterflow export-code --project $project_id --dest "ff-app" --token [TOKEN]
+    flutterflow export-code --project $project_id --dest "ff-app" --token [TOKEN] --include-assets
 else
     # Initialize a flag to indicate that prerequisites are met
     prerequisites_met=true
@@ -29,7 +34,6 @@ else
         echo "First, you need to install FlutterFlow CLI - https://pub.dev/packages/flutterflow_cli"
         prerequisites_met=false
     fi
-
 
     # Check for connected devices via adb
     if adb devices | grep -q 'device$'; then
@@ -43,15 +47,11 @@ else
     # Assuming prerequisites_met is a string 'true' or 'false'
     if [ "$prerequisites_met" = "true" ]; then
         echo "Attempting to export code with FlutterFlow CLI..."
-        flutterflow export-code --project $project_id --dest "ff-app" --token [TOKEN]
+        flutterflow export-code --project $project_id --dest "ff-app" --token [TOKEN] --include-assets
 
         # Assuming 'ff-app' directory is successfully created
         cd ff-app || { echo "Failed to change directory to ff-app"; exit 1; }
         echo "Current directory after moving to 'ff-app': $(pwd)"
-
-        project_prefix=$(echo "$project_id" | awk -F'-' '{print $1}')
-        project_dir_name=$(find . -maxdepth 1 -type d -name "${project_prefix}*" | head -n 1)
-        project_dir_name=${project_dir_name#./}
 
         if [ -z "$project_dir_name" ]; then
             echo "No directory matching the prefix '$project_prefix' found."

a7e6b9628cc94f1ddccdb5f458c45e6842096f97
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2025-05-14 13:51:32 +0300

    Add hideKeyboard function to manage keyboard visibility

---
+++ Custom Actions/hide-keyboard.dart
@@ -0,0 +1,13 @@
+// YouTube channel - https://www.youtube.com/@flutterflowexpert
+// paid video - no
+// Join the Klaturov army - https://www.youtube.com/@flutterflowexpert/join
+// Support my work - https://github.com/sponsors/bulgariamitko
+// Website - https://bulgariamitko.github.io/flutterflowtutorials/
+// You can book me as FF mentor - https://calendly.com/bulgaria_mitko
+// GitHub repo - https://github.com/bulgariamitko/flutterflowtutorials
+// Discord channel - https://discord.gg/G69hSUqEeU
+
+Future hideKeyboard(BuildContext context) async {
+  // This will hide the keyboard by removing focus from the current input field
+  FocusScope.of(context).unfocus();
+}
